5c3e3096b2bbf807b3dd52ee1c1572369649dc99
jmchang35 <89932840+jmchang35@users.noreply.github.com>
2022-06-14 15:30:10 -0400

    plaintext

--- morse/index.js
+++ morse/index.js
@@ -2,13 +2,13 @@ const morse = require("morse-code-converter");
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const english = req.query.plaintext;
+    const plaintext = req.query.plaintext;
     var code = "";
-    if (typeof english === 'undefined' || english === "") {
+    if (typeof plaintext === 'undefined' || plaintext === "") {
         code = "Please enter some text to convert!"
     }
     else {
-        code = morse.textToMorse(english);
+        code = morse.textToMorse(plaintext);
     }
     
 
