2b8c4f143dbd9d7ed452f8ffb9c271e8435cb6b4
jmchang35 <89932840+jmchang35@users.noreply.github.com>
2022-07-10 14:58:30 -0400

    ultimate cat invasion 2

--- catzapp/index.js
+++ catzapp/index.js
@@ -7,13 +7,16 @@ button.addEventListener("click", async function() {
     const name3 = document.getElementById("name3").value;
     const name4 = document.getElementById("name4").value;
 
+
     const AZURE_URL = "https://bitproj1.azurewebsites.net/api/twocatz?code=SqI1Ja9KEXBgetNmz2E4gjXNm-N8ZW3LaRMwKX6zdA-zAzFuhIyUHA==";
     const fetch_url = `${AZURE_URL}&name1=${name1}&name2=${name2}&name3=${name3}&name4=${name4}`;
+
 
     const resp = await fetch(fetch_url, {
         method: "GET",
     });
 
+    
     const data = await resp.json();
     
     setSourceOfBase64("image1", data.cat1);
@@ -22,7 +25,6 @@ button.addEventListener("click", async function() {
     setSourceOfBase64("image4", data.cat4);
    
 })
-
 
 function setSourceOfBase64(id, base64String) {
     document.getElementById(id).src = "data:image/png;base64," + base64String;
