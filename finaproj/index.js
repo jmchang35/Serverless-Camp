8cef32d7e09c59cd32f6f388af2bebfe90145b05
jmchang35 <89932840+jmchang35@users.noreply.github.com>
2022-08-06 17:27:31 -0400

    made trigger and added query

--- finaproj/index.js
+++ finaproj/index.js
@@ -1,4 +1,3 @@
-
 const CosmosClient = require("@azure/cosmos").CosmosClient;
 const config = require("./config");
 const dbContext = require("./data/databaseContext");
@@ -6,6 +5,24 @@ const dbContext = require("./data/databaseContext");
 var result;
 var user_coords = new Array(0,0);
 var distance;
+
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
+
+
+
 
 async function main() {
     const { endpoint, key, databaseId, containerId } = config;
