b43e51086c912d6ecb149022e2633e373e64ad7c
jmchang35 <89932840+jmchang35@users.noreply.github.com>
2022-08-07 00:11:31 -0400

    commits

--- finaproj/config.js
+++ finaproj/config.js
@@ -3,9 +3,9 @@
 const config = {
     endpoint: "https://bathroom-locations.documents.azure.com:443/",
     key: "6yq39gKGD799SDPDtSu916e1JYx9FtARfFvOqQVvRWVvz1T7RIuT3iGbP1JahCinEcenhCKhos5x0qFxyBcgvA==",
-    databaseId: "Tasks",
-    containerId: "Items",
-    partitionKey: { kind: "Hash", paths: ["/category"] }
+    databaseId: "bathroomsdatabaseid",
+    containerId: "bathroomscontainerid",
+    partitionKey: { kind: "Hash", paths: ["/id"] }
   };
   
   module.exports = config;
