a9c082670cd7848f3e334adc866c659a0a51921e
jmchang35 <89932840+jmchang35@users.noreply.github.com>
2022-08-09 22:16:25 -0400

    test code for comparing distances of bathrooms

--- project/app.js
+++ project/app.js
@@ -4,6 +4,8 @@ const config = require("./config");
 
 async function main() {
   
+
+  
   // <CreateClientObjectDatabaseContainer>
   const { endpoint, key, databaseId, containerId } = config;
 
@@ -12,28 +14,46 @@ async function main() {
   const database = client.database(databaseId);
   const container = database.container(containerId);
 
-
-  
+  lon = "-86.91127264673787";
+  lat = "40.423789700013344";
+  var distance = 0;
+  var id = "";
+  var name = "";
+  let first = true;
   try {
     // <QueryItems>
     console.log(`Querying container: Items`);
 
     // query to return all items
     const querySpec = {
-      query: "SELECT * from c"
+      query: "SELECT b, ST_DISTANCE(b.location, {'type': 'Point', 'coordinates':[" + lon + "," + lat + "]}) myDistance FROM bathroomscontainerid b WHERE ST_DISTANCE(b.location, {'type': 'Point', 'coordinates':[" + lon + "," + lat + "]}) < 100"
     };
-    
+
     // read all items in the Items container
     const { resources: items } = await container.items
       .query(querySpec)
       .fetchAll();
 
     items.forEach(item => {
-      console.log(`${item.id} - ${item.name}`);
+        if (first) {
+          id = item.b.id;
+          name = item.b.name;
+          distance = item.myDistance;
+          first = false;
+        } else {
+          if (item.myDistance < distance) {
+            id = item.b.id;
+            name = item.b.name;
+            distance = item.myDistance;
+          }
+        }
+        
+      console.log(`${item.b.id} - ${item.b.name} - ${item.myDistance}`);
+      console.log(id + name + distance);
     });
     // </QueryItems>
-    
-    
+
+
   } catch (err) {
     console.log(err.message);
   }
